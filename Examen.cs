f3236d54faa2161a64f49bb3a44782f576221a02
Cesar <ca614095@gmail.com>
2023-10-06 08:47:56 -0700

    Agregar archivos de proyecto.

---
+++ Examen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenEstructuraDatos
+{
+
+    //------------------NUMEROS SECUENCIALES----------------
+    public class Examen
+    {
+        //public void numerosSecuencia(int num1, int num2)
+        //{
+        //    if (num1 == num2)
+        //    {
+        //        Console.WriteLine("Los numeros son iguales");
+        //    }
+
+        //    for (int i = num1; i <= num2; i++)
+        //    {
+        //        Console.WriteLine($"La secuencia de los numeros es {i}");
+        //    }
+
+        //    for (int i = num1; i >= num2; i--)
+        //    {
+        //        Console.WriteLine($"La secuenciua de los numeros es {i}");
+        //    }
+
+        //}
+
+
+
+
+        //-----------------TABLAS DE MULTIPLICAR-----------------
+
+        public void tablaMultiplicar(int num1)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num1} X {i} = {num1 * i}");
+            }
+        }
+
+
+
+
+        //----------------------CURP---------------------
+
+        //public void (string curp)
+        //{
+
+        //}
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ExamenEstructuraDatos;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //------------------NUMEROS SECUENCIALES----------------
+        //Examen secuenciaNumeros = new Examen();
+        //Console.WriteLine("ingrese el primer numero");
+        //int numero = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("ingrese el segundo numero");
+        //int numero2 = Convert.ToInt32(Console.ReadLine());
+        //secuenciaNumeros.numerosSecuencia(numero, numero2);
+
+
+        //-----------------TABLAS DE MULTIPLICAR-----------------
+        Examen multiplicarNumero = new Examen();
+        Console.WriteLine("Ingrese el numero a multiplicar");
+        int multiplicar = Convert.ToInt32(Console.ReadLine());
+        multiplicarNumero.tablaMultiplicar(multiplicar);
+
+    }
+
+
+}
